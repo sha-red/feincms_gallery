694561c52439941edc357ab5f4e525c6b2387807
Erik Stein <erik@classlibrary.net>
2017-02-16 14:31:04 +0100

    Added internal_name field to Gallery.

--- gallery/models.py
+++ gallery/models.py
@@ -15,8 +15,9 @@ __all__ = ['Gallery', 'GalleryMediaFile', 'DEFAULT_SPECS']
 
 @python_2_unicode_compatible
 class Gallery(models.Model):
-    title = models.CharField(_("Title"), max_length=500)
-    title_en = models.CharField(_("Title (english"), max_length=500, null=True, blank=True)
+    internal_name = models.CharField(_("Interne Bezeichnung"), max_length=500, help_text=_("Erscheint nicht auf der Website"))
+    title = models.CharField(_("Title"), max_length=500, null=True, blank=True)
+    title_en = models.CharField(_("Title (english)"), max_length=500, null=True, blank=True)
     description_de = models.TextField(_("Description"), null=True, blank=True)
     description_en = models.TextField(_("Description (english"), null=True, blank=True)
     images = models.ManyToManyField(MediaFile, verbose_name=_("Gallery Images"), through='GalleryMediaFile')  # TODO Restrict to type='image'?
@@ -26,7 +27,7 @@ class Gallery(models.Model):
         verbose_name_plural = _("Galleries")
 
     def __str__(self):
-        return self.title
+        return self.internal_name
 
     def ordered_images(self):
         return self.images.select_related().all() \
